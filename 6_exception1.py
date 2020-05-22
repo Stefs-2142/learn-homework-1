491526e20aed2ff4afcdf51cae64ebc5f693a073
Stepan Bubnov <exectrogod@gmail.com>
2020-05-23 01:02:02 +0300

    Выполнил 6-ое задание

--- 6_exception1.py
+++ 6_exception1.py
@@ -9,12 +9,23 @@
   и завершала работу при помощи оператора break
     
 """
+q_a_dict = {"Что делаешь?":"Программирую","Как дела?":"Нормально","Выходил сегодня на улицу?":"Нет, сидел дома.",
+            "Как ты себя чувствуешь?":"Отлично!"}
 
-def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+
+def ask_user_dict(user_dict):
+  
+  
+    while True:
+      try:
+        ask = input()
+        if ask in q_a_dict.keys():
+          return q_a_dict[ask]
+        else:
+          continue
+      except KeyboardInterrupt:
+        print('Пока')
+        break
     
 if __name__ == "__main__":
-    ask_user()
+    ask_user_dict(q_a_dict)
