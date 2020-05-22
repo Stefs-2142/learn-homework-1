99d7098707ba7b8f80234fe77bafe2c5addeb33e
Stepan Bubnov <exectrogod@gmail.com>
2020-05-23 01:33:36 +0300

    Выполнил 7-ое задание.

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,14 +13,22 @@
 """
 
 def get_summ(num_one, num_two):
-    """
-    Замените pass на ваш код
-    """
-    pass
+      try:
+        num_one = int(num_one)
+        num_two = int(num_two)
+        summ = num_two + num_two
+        return summ
+      except ValueError:
+        print ('Вы ввели не число.')
+      
+    
     
 if __name__ == "__main__":
-    print(get_summ(2, 2))
-    print(get_summ(3, "3"))
-    print(get_summ("4", "4"))
-    print(get_summ("five", 5))
-    print(get_summ("six", "шесть"))
+  print(get_summ(2, 2))                        
+  print(get_summ(3, "3"))                     
+  print(get_summ("4", "4"))                   
+  print(get_summ("five", 5))                     
+  print(get_summ("six", "шесть"))    
+
+  """Как переписать функцию, чтобы она выводила результат без None?"""          
+                                                
