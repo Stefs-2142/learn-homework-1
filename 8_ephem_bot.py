5498d2bfdf09cef11b4f8bc4730bebeef9f3fe74
Stepan Bubnov <exectrogod@gmail.com>
2020-05-23 15:48:26 +0300

    Выполнил 8-ое задание.

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,47 +12,54 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+import settings
 import logging
-
+import ephem
+from datetime import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-)
+#Расширенное логирование, с датой и временем.
+logging.basicConfig(filename='bot.log', level=logging.INFO,format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',)
 
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
-
-
-def greet_user(bot, update):
+def greet_user(update, context):
     text = 'Вызван /start'
-    print(text)
     update.message.reply_text(text)
 
+def get_ephem_info(update, context):
+    """Определяем в каком  совездии сегодня находится объект"""
 
-def talk_to_me(bot, update):
+    entered_planet = update.message.text.split()[-1].capitalize()  # Получаем название планеты из пользовательского ввода команды /planet {planet_name}
+    if hasattr(ephem, entered_planet):                             # Проверяем есть ли введённая планета
+        planet = getattr(ephem, entered_planet)
+        
+        today = datetime.today()
+        planet_loc = ephem.constellation(planet(today))    # Запрашиваем локацию на сегодняшний день
+        formated_planet_loc = (f'{entered_planet} is in the constellation: {planet_loc[-1]}')
+        update.message.reply_text(formated_planet_loc)
+    else:
+        update.message.reply_text('Whoops try again')    
+
+def talk_to_me(update, context):
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
- 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet",get_ephem_info))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+
+    logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
-       
 
 if __name__ == "__main__":
     main()
+
