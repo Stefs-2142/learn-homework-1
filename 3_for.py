91b33717bff30a97c0e8b9a8db96d9470b1b47b8
Stepan Bubnov <exectrogod@gmail.com>
2020-05-24 20:09:02 +0300

    Change funcion/variable name

--- 3_for.py
+++ 3_for.py
@@ -13,27 +13,27 @@
 score_dicts = [{'school_class': '4a', 'scores': [3,4,4,5,2]},{'school_class': '11a', 'scores': [3,4,5,4,5,5,4]}, 
     {'school_class': '8b', 'scores': [2,4,3,4,2,5,3]}, {'school_class': '2a', 'scores': [5,4,5,4,5,5,4]}]
 
-def get_average_sc_score():
+def print_average_sc_score():
   points = 0
   total_students = 0
-  for sc in score_dicts:
-    points += sum(sc['scores'])
-    total_students += len(sc['scores'])
+  for class_score in score_dicts:
+    points += sum(class_score['scores'])
+    total_students += len(class_score['scores'])
   average_sc_score = points / total_students
-  print ('Avarage score in school is: ' + str(round(average_sc_score,2)))
+  print('Avarage score in school is: ' + str(round(average_sc_score,2)))
 
-def get_average_class():
-  points = 0
-  for sc in score_dicts:
-    points += sum(sc['scores'])
-    average_class_score = points / len(sc['scores'])
-    print ('In class ' + sc['school_class'] + f' - avarege point is: {round(average_class_score,2)}')
-    points = 0    
+def print_average_class():
+  for class_score in score_dicts:
+    points = 0
+    points = sum(class_score['scores'])
+    average_class_score = points / len(class_score['scores'])
+    print('In class ' + class_score['school_class'] + f' - avarege point is: {round(average_class_score,2)}')
+        
     
 def main():
 
-  get_average_sc_score()
-  get_average_class()
+  print_average_sc_score()
+  print_average_class()
 
     
     
